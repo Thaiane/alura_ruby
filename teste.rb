6323a1e6278ed2beccf07a2b0cc5f95da7855204
Thaiane Braga <thaianefbraga@gmail.com>
2017-08-27 23:02:31 -0300

    Aula 3

--- teste.rb
+++ teste.rb
@@ -1,35 +1,44 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts
-puts
-puts
-puts
-puts
-puts
-puts "Começaremos o jogo para você, " + nome
+def boas_vindas
 
-for tentativa in 1..3 do
+  puts "Bem vindo ao jogo da adivinhação"
+  puts "Qual é o seu nome?"
+  nome = gets
+  puts "\n\n\n\n\nComeçaremos o jogo para você, " + nome
+
+end
+
+boas_vindas
+
+def sorteia_numero_secreto
   puts "Escolhendo um número secreto entre 0 e 200..."
   numero_secreto = 175
   puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-  puts
-  puts
-  puts
-  puts
-  puts "Tentativa " + tentativa.to_s
-  puts "Entre com o numero"
-  chute = gets
-  puts "Será que acertou? Você chutou " + chute
+  puts "\n\n\n"
+  numero_secreto
+end
 
+  numero_sorteado = sorteia_numero_secreto
 
-  acertou = numero_secreto == chute.to_i
+def pede_um_numero(tentativa)
+    puts "\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
+    chute
+end
+
+for tentativa in 1..3 do
+
+  chute = pede_um_numero(tentativa)
+
+  acertou = numero_sorteado == chute.to_i
 
   if acertou
     puts "Acertou!"
     break
   else
-    if (numero_secreto - chute.to_i) > 0
+    if (numero_sorteado - chute.to_i) > 0
       puts "Chute menor que o número secreto \n"
     else 
       puts "Chute maior que o número secreto \n"
