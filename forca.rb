377b14615e5a7d5787a6cfa890534c0dfc625b19
Thaiane Braga <thaianefbraga@gmail.com>
2017-09-09 21:11:17 -0300

    jogo da forca
    Signed-off-by: Thaiane Braga <thaianefbraga@gmail.com>

--- forca.rb
+++ forca.rb
@@ -19,3 +19,52 @@ def nao_quer_jogar?
     quero_jogar = gets.strip
     nao_quero_jogar = quero_jogar.upcase == "N"
 end
+
+def joga(nome)
+    palavra_secreta = sorteia_palavra_secreta
+
+    erros = 0
+    chutes = []
+    pontos_ate_agora = 0
+
+    while erros < 5
+        chute = pede_um_chute chutes, erros
+        chutes << chute
+
+        # colocar as regras de acerto e erro aqui!
+
+    end
+
+    puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def pede_um_chute(chutes, erros)
+    puts "\n\n\n\n"
+    puts "Erros até agora: #{erros}"
+    puts "Chutes até agora: #{chutes}"
+    puts "Entre com a letra ou palavra"
+    chute = gets.strip
+    puts "Será que acertou? Você chutou #{chute}"
+    chute
+end
+
+nome = boas_vindas
+
+loop do
+    chutou_uma_unica_letra = chute.size == 1
+	if chutou_uma_unica_letra
+
+	else
+   		acertou = chute == palavra_secreta
+   		 if acertou
+        	puts "Parabéns! Acertou!"
+        	pontos_ate_agora += 100
+        	break
+    	else
+        	puts "Que pena... errou!"
+        	pontos_ate_agora -= 30
+        	erros += 1
+    	end
+	end
+    break if nao_quer_jogar?
+end
